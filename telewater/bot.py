544eecea26f3b4b6c3ff971fffa2e2f9a965a022
rgaliveeti <74495581+rgaliveeti@users.noreply.github.com>
2022-06-24 11:28:20 +0530

    Update bot.py

--- telewater/bot.py
+++ telewater/bot.py
@@ -109,7 +109,7 @@ async def watermarker(event):
     )
     await event.client.send_file(event.sender_id, out_file,caption=text)
     try:
-        await event.client.edit_file(event.sender_id,id1,out_file,caption=text)
+        await event.client.delete_file(event.sender_id,id1)
     except:
         pass
     cleanup(org_file, out_file)
